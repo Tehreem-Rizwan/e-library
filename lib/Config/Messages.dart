f35646743f24b9c736510fb031ecade0691874d6
Tehreem Rizwan <its.tehreem2830@gmail.com>
2024-12-27 21:44:55 +0500

    committed

--- lib/Config/Messages.dart
+++ lib/Config/Messages.dart
@@ -1,26 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 void successMessage(String text) {
-  Fluttertoast.showToast(
-    msg: text,
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 1,
-    backgroundColor: Color(0xff00C236),
-    textColor: Colors.white,
-    fontSize: 15.0,
+  EasyLoading.showToast(
+    text,
+    toastPosition: EasyLoadingToastPosition.bottom,
   );
 }
 
 void errorMessage(String text) {
-  Fluttertoast.showToast(
-    msg: text,
-    toastLength: Toast.LENGTH_SHORT,
-    gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 1,
-    backgroundColor: Color(0xffF24E1E),
-    textColor: Colors.white,
-    fontSize: 15.0,
+  EasyLoading.showToast(
+    text,
+    toastPosition: EasyLoadingToastPosition.bottom,
   );
 }
